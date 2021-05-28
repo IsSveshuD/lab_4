be6ba41519212c3932de1467aba8caf1aed32be4
IsSveshuD <ivan.ivan.lysenko@gmail.com>
2021-05-28 11:15:28 +0300

    Add all

--- primer5.py
+++ primer5.py
@@ -1,11 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import math
 import sys
 
 # Постоянная Эйлера.
 EULER = 0,5772156649015328606
 # Точность вычислений.
-ESP = 1e-10
-
+EPS = 1e-10
 
 if __name__ == '__main__':
     x = float(input("Value of x? "))
@@ -17,10 +19,10 @@ if __name__ == '__main__':
     S, k = a, 1
 
     # Найти сумму членов ряда.
-    while math.fabs(a) > ESP:
+    while math.fabs(a) > EPS:
         a *= x * k / (k + 1) ** 2
         S += a
         k += 1
 
-    #Вывести значение функции.
-    print(f"Ei({x}) - {EULER + math.log(math.fabs(x)) + S}")
+    # Вывести значение функции.
+    print(f"Ei({x}) = {EULER + math.log(math.fabs(x)) + S}")
